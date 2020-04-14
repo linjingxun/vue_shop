b4d6a15dee3389c412a2dd7e491b44afb351191e
ljx <147@qq.com>
2020-04-14 21:24:00 +0800

    第一次

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/login">登录</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body{
+  background-color: #2b4b6b;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e6ebf0;
+  margin-top: 60px;
+}
+</style>
